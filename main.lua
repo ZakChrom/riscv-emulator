db7d00824798143ee27be2968d92122ee3ed4bb4
Blendi-Goose <87442375+Blendi-Goose@users.noreply.github.com>
2025-08-14 21:31:37 +0200

    a bunch of fun parts of fun instructions (yay)

--- main.lua
+++ main.lua
@@ -10,17 +10,53 @@ while true do
 		local opcode = Num.getBits(inst, 0,6)
 
 		if opcode == 55 then -- 0b0110111, LUI
+			local rd = Num.getBits(inst, 7, 11)
+			local imm = Num.getBits(inst, 12, 31)
 		elseif opcode == 23 then -- 0010111, AUIPC
+			local rd = Num.getBits(inst, 7, 11)
+			local imm = Num.getBits(inst, 12, 31)
 		elseif opcode == 111 then -- 0b1101111, JAL
+			local rd = Num.getBits(inst, 7, 11)
 		elseif opcode == 103 then -- 0b1100111, JALR
+			local funct3 = Num.getBits(inst, 12, 14)
 		elseif opcode == 99 then -- 0b1100011, BRANCH
+			local funct3 = Num.getBits(inst, 12, 14)
+
+			local rs1 = Num.getBits(inst, 15, 19)
+			local rs2 = Num.getBits(inst, 20, 24)
 		elseif opcode == 3 then -- 0b0000011, LOAD
+			local rd = Num.getBits(inst, 7, 11)
+			local funct3 = Num.getBits(inst, 12, 14)
+			local rs1 = Num.getBits(inst, 15, 19)
 		elseif opcode == 35 then -- 0b0100011, STORE
+			local funct3 = Num.getBits(inst, 12, 14)
+			local rs1 = Num.getBits(inst, 15, 19)
+			local rs2 = Num.getBits(inst, 20, 24)
 		elseif opcode == 19 then -- 0b0010011, immediate register stuff
+			local funct3 = Num.getBits(inst, 12, 14)
+			local rd = Num.getBits(inst, 7, 11)
+			local rs1 = Num.getBits(inst, 15, 19)
 		elseif opcode == 51 then -- 0b0110011, register register stuff (but also mul/div from `m`)
+			local rd = Num.getBits(inst, 7, 11)
+			local funct3 = Num.getBits(inst, 12, 14)
+			local rs1 = Num.getBits(inst, 15, 19)
+			local rs2 = Num.getBits(inst, 20, 24)
+
+			local funct7 = Num.getBits(inst, 25, 31)
 		elseif opcode == 15 then -- 0b0001111, fence.i
 		elseif opcode == 115 then -- 0b1110011 system (ecall, ebreak, Zicsr stuff)
+			local funct3 = Num.getBits(inst, 12, 14)
+			local rd = Num.getBits(inst, 7, 11)
+			local rs1 = Num.getBits(inst, 15, 19)
 		elseif opcode == 47 then -- 0b0101111, AMO
+			local rd = Num.getBits(inst, 7, 11)
+			local rs1 = Num.getBits(inst, 15, 19)
+			local rs2 = Num.getBits(inst, 20, 24)
+			local funct3 = Num.getBits(inst, 12, 14)
+			local funct5 = Num.getBits(inst, 27, 31)
+
+			local rl = Num.getBits(25,25)
+			local aq = Num.getBits(26,26)
 		end
 	else
 		-- raise illegal instruction... or handle c extension if we do that... or other stuff
