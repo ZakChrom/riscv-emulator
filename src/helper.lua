0a730c315469d27622b408b3a076608eda66f4b4
Blendi-Goose <87442375+Blendi-Goose@users.noreply.github.com>
2025-08-14 20:06:30 +0200

    more num function

--- src/helper.lua
+++ src/helper.lua
@@ -15,3 +15,35 @@ function Num.signed(a)
 
 	return a;
 end
+
+function Num.rshift(a, amount)
+	assert(amount >= 0)
+	return math.floor(a / (2^amount))
+end
+
+function Num.lshift(a, amount)
+	assert(amount >= 0)
+	a = a * 2^amount
+	return a % (2^32)
+end
+
+function Num.getBits(a, pos1, pos2) -- starts at 0. example: getBits(0b1010, 0, 1) == 0b10, getBits(0b1010, 1,2) == 0b01
+	a = Num.rshift(a, pos1)
+
+	local offset = pos2-pos1
+	a = a % (2^(offset + 1))
+
+	return a
+end
+
+function Num.multiply(a,b) -- returns the less significant part first.
+	-- here for precision concerns.
+	-- for now: bullshit method
+	-- TODO: method that doesn't lose precision.
+	local mult = a*b
+
+	local ret1 = math.floor(mult % (2^32))
+	local ret2 = math.floor(mult / (2^32))
+
+	return ret1, ret2
+end
