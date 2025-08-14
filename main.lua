4133f43f1371a787643277530faa81f5277f1afd
Blendi-Goose <87442375+Blendi-Goose@users.noreply.github.com>
2025-08-14 20:45:48 +0200

    me when the start of the thing

--- main.lua
+++ main.lua
@@ -5,5 +5,12 @@ local pc = 0
 
 while true do
 	local inst = Memory.read(pc, 4)
+
+	if Num.getBits(inst, 0,1) == 3 then -- normal 32-bit instruction
+		
+	else
+		-- raise illegal instruction... or handle c extension if we do that... or other stuff
+	end
+
 	pc = pc + 4
 end
