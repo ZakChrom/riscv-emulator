5ff2dff1a7d9bb4ceb972baa07479fed1efdb8a2
ZakChrom <88797993+ZakChrom@users.noreply.github.com>
2025-08-14 20:28:51 +0300

    fix memory

--- src/memory.lua
+++ src/memory.lua
@@ -1,15 +1,15 @@
 Memory = {
-    memory = string.rep(string.byte(0), 1024)
+	memory = string.rep(string.byte(0), 1024)
 }
 
 ---@param addr integer
 ---@return integer
 function Memory.read(addr)
-    return Memory.memory:byte(addr, addr)
+	return Memory.memory:byte(addr + 1, addr + 1)
 end
 
 ---@param addr integer
 ---@param v integer
 function Memory.write(addr, v)
-    Memory.memory = Memory.memory:sub(1, addr - 1) .. v .. Memory.memory:sub(addr + 1)
+	Memory.memory = Memory.memory:sub(1, addr - 1 + 1) .. string.byte(v) .. Memory.memory:sub(addr + 1 + 1)
 end
