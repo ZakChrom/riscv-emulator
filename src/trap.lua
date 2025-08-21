5de99ca0fc7d69ab0007f127d0c06b6aca79e43a
ZakChrom <88797993+ZakChrom@users.noreply.github.com>
2025-08-21 23:45:29 +0300

    better trap

--- src/trap.lua
+++ src/trap.lua
@@ -4,6 +4,20 @@ function Trap.raise(trap, value)
 	assert(CSRs.write(0x342, trap)); -- mcause
 	assert(CSRs.write(0x343, value)); -- mtval
 	assert(CSRs.write(0x341, Hart.pc)) -- mepc
-	Hart.pc = Num.clear(assert(CSRs.read(0x305)), 3) -- mtvec
+
+	local mstatus = assert(CSRs.read(0x300))
+	local mie = Num.getBits(mstatus, 3, 3)
+	assert(CSRs.write(0x300, Num.clear(mstatus, 6272) + (mie * 128) + (Hart.mode * 2048)))
+	Hart.mode = Mode.Machine
+
+	local mtvec = assert(CSRs.read(0x305))
+	local base = Num.clear(mtvec, 3)
+	local mode = Num.getBits(mtvec, 0, 1)
+	assert(mode == 0 or mode == 1)
+	if mode == 1 then
+		Hart.pc = base + (4 * Num.getBits(trap, 0, 30))
+	else
+		Hart.pc = base
+	end
 	Hart.pc_inc_amount = 0
 end
